374800f89a630b490d2220d0c58a56650a028edc
Prateek Pawan <prateekpawan@Prateeks-MacBook-Pro.local>
2023-04-23 22:14:48 +0530

    adding participant view to admin panel for contest

---
+++ trading-app/src/layouts/contests/data/contestParticipantsData.js
@@ -0,0 +1,14 @@
+export default function ContestRewards() {
+
+    return {
+  
+      columns: [
+          { Header: "Name", accessor: "name", align: "center" },
+          { Header: "Portfolio", accessor: "portfolio", align: "center" },
+          { Header: "Status", accessor: "status", align: "center" },
+        ],
+  
+        rows: []
+  
+    };
+  }
