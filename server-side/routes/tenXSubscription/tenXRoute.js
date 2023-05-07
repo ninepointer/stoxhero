bd4fff2dbcb40ca1128b1bbf9656ea371c7bd46d
Prateek Pawan <prats24@gmail.com>
2023-05-07 20:15:33 +0530

    hello

--- server-side/routes/tenXSubscription/tenXRoute.js
+++ server-side/routes/tenXSubscription/tenXRoute.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
-const multer = require('multer');
-const aws = require('aws-sdk');
-const {createTenXSubscription, editFeature} = require("../../controllers/tenXSubscriptionController");
+const {createTenXSubscription, editFeature, getActiveTenXSubs} = require("../../controllers/tenXSubscriptionController");
 const authentication = require("../../authentication/authentication")
 
 // router.route('/userDetail').post(upload.array("files"), getUploadsApplication);
 router.route('/create').post(authentication, createTenXSubscription);
+router.route('/active').get(authentication, getActiveTenXSubs);
 router.route('/:id').patch(authentication, editFeature);
 
 module.exports = router;
