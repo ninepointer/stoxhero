4d24a7cd6fb562573e26df463d391b58bc388577
someone <vvv201214@gmail.com>
2023-05-07 20:15:49 +0530

    tenx

--- trading-app/src/layouts/career/createCareerForm.js
+++ trading-app/src/layouts/career/createCareerForm.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import {useContext, useState} from "react";
+// import * as React from 'react';
+import {useState} from "react";
 // import { useForm } from "react-hook-form";
 // import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -11,10 +11,12 @@ import MDButton from "../../components/MDButton"
 import { CircularProgress } from "@mui/material";
 import MDSnackbar from "../../components/MDSnackbar";
 import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import { useNavigate, useLocation } from "react-router-dom";
+import RolesAndResponsibilities from './data/roleAndRespData';
+import { IoMdAddCircle } from 'react-icons/io';
 
 function Index() {
 
@@ -27,10 +29,16 @@ function Index() {
     const [saving,setSaving] = useState(false)
     const [creating,setCreating] = useState(false)
     const navigate = useNavigate();
+    const [newObjectId, setNewObjectId] = useState("");
+    const [updatedDocument, setUpdatedDocument] = useState([]);
+
     const [formState,setFormState] = useState({
         jobTitle:'',
         jobDescription:'',
-        rolesAndResponsibilities:'',
+        rolesAndResponsibilities: {
+          orderNo: "",
+          description: ""
+        },
         jobType:'',
         jobLocation:'',
         status:''
@@ -69,7 +77,7 @@ function Index() {
           // console.log("invalid entry");
       } else {
           openSuccessSB("Career Created",data.message)
-          // setNewObjectId(data.data._id)
+          setNewObjectId(data.data._id)
           setIsSubmitted(true)
           // console.log("setting linked contest rule to: ",data.data.contestRule)
           // setLinkedContestRule(data?.data?.contestRule)
@@ -77,6 +85,41 @@ function Index() {
           // setContestData(data.data)
           setTimeout(()=>{setCreating(false);setIsSubmitted(true)},500)
         }
+    }
+
+    async function onAddFeature(e,childFormState,setChildFormState){
+      e.preventDefault()
+      setSaving(true)
+      if(!childFormState?.orderNo || !childFormState?.description){
+          setTimeout(()=>{setCreating(false);setIsSubmitted(false)},500)
+          return openErrorSB("Missing Field","Please fill all the mandatory fields")
+      }
+      const {orderNo, description} = childFormState;
+    
+      const res = await fetch(`${baseUrl}api/v1/career/${newObjectId}`, {
+          method: "PATCH",
+          credentials:"include",
+          headers: {
+              "content-type" : "application/json",
+              "Access-Control-Allow-Credentials": true
+          },
+          body: JSON.stringify({
+            rolesAndResponsibilities:{orderNo, description}
+          })
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.status === 422 || data.error || !data) {
+          openErrorSB("Error","data.error")
+      } else {
+          setUpdatedDocument(data?.data);
+          openSuccessSB("New Role and Responsibility Added","New Role and Responsibility line item has been added in the career")
+          setTimeout(()=>{setSaving(false);setEditing(false)},500)
+          setChildFormState(prevState => ({
+              ...prevState,
+              rolesAndResponsibilities: {}
+          }))
+      }
     }
   
 
@@ -302,6 +345,60 @@ function Index() {
                     </>
                     )}
             </Grid>
+
+            {(isSubmitted || id) && !editing && 
+                <Grid item xs={12} md={6} xl={12}>
+                    
+                    <Grid container spacing={1}>
+
+                    <Grid item xs={12} md={6} xl={12} mt={-3} mb={-1}>
+                    <MDTypography variant="caption" fontWeight="bold" color="text" textTransform="uppercase">
+                        Add Roles & Responsibilities
+                    </MDTypography>
+                    </Grid>
+                    
+                    <Grid item xs={12} md={1.35} xl={2.7}>
+                        <TextField
+                            id="outlined-required"
+                            label='Order No. *'
+                            fullWidth
+                            type="number"
+                            // value={formState?.features?.orderNo}
+                            onChange={(e) => {setFormState(prevState => ({
+                                ...prevState,
+                                orderNo: e.target.value
+                            }))}}
+                        />
+                    </Grid>
+        
+                    <Grid item xs={12} md={1.35} xl={2.7}>
+                        <TextField
+                            id="outlined-required"
+                            label='Description *'
+                            fullWidth
+                            type="text"
+                            // value={formState?.features?.description}
+                            onChange={(e) => {setFormState(prevState => ({
+                                ...prevState,
+                                description: e.target.value
+                            }))}}
+                        />
+                    </Grid>
+            
+                    <Grid item xs={12} md={0.6} xl={1.2} mt={-0.7}>
+                        <IoMdAddCircle cursor="pointer" onClick={(e)=>{onAddFeature(e,formState,setFormState)}}/>
+                    </Grid>
+    
+                    </Grid>
+    
+                </Grid>}
+
+                {(isSubmitted || id) && <Grid item xs={12} md={12} xl={12} mt={2}>
+                    <MDBox>
+                        <RolesAndResponsibilities updatedDocument={updatedDocument}/>
+                    </MDBox>
+                </Grid>}
+
          </Grid>
 
           {renderSuccessSB}
---
+++ trading-app/src/layouts/career/data/roleAndRespData.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import DataTable from "../../../examples/Tables/DataTable";
+import MDButton from "../../../components/MDButton"
+import MDBox from "../../../components/MDBox"
+import MDTypography from "../../../components/MDTypography"
+import Card from "@mui/material/Card";
+
+
+export default function RolesAndResponsibilities({updatedDocument}) {
+    console.log("updatedDocument", updatedDocument)
+    let columns = [
+        { Header: "Order No.", accessor: "orderNo", align: "center" },
+        { Header: "Description", accessor: "description", align: "center" },
+        { Header: "Edit", accessor: "edit", align: "center" },
+        { Header: "Delete", accessor: "delete", align: "center" },
+      ]
+
+    let rows = []
+
+updatedDocument?.rolesAndResponsibilities?.map((elem)=>{
+  let featureObj = {}
+  featureObj.orderNo = (
+    <MDTypography component="a" variant="caption" color="text" fontWeight="medium">
+      {elem.orderNo}
+    </MDTypography>
+  );
+  featureObj.description = (
+    <MDTypography component="a" variant="caption" color="text" fontWeight="medium">
+      {elem.description}
+    </MDTypography>
+  );
+  featureObj.edit = (
+    <MDButton component="a" variant="caption" color="text" fontWeight="medium">
+      Edit
+    </MDButton>
+  );
+  featureObj.delete = (
+    <MDButton component="a" variant="caption" color="text" fontWeight="medium">
+      Delete
+    </MDButton>
+  );
+
+  rows.push(featureObj)
+})
+
+  return (
+    <Card>
+      <MDBox display="flex" justifyContent="space-between" alignItems="center">
+        <MDBox width="100%" display="flex" justifyContent="center" alignItems="center" sx={{backgroundColor:"lightgrey",borderRadius:"2px"}}>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
+            Feature added!
+          </MDTypography>
+        </MDBox>
+      </MDBox>
+      <MDBox mt={1}>
+        <DataTable
+          table={{ columns, rows }}
+          showTotalEntries={false}
+          isSorted={false}
+          // noEndBorder
+          entriesPerPage={false}
+        />
+      </MDBox>
+    </Card>
+  );
+}
+
