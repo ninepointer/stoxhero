fa08204d8237dae8d59a05760691288af5a43187
Prateek Pawan <prats24@gmail.com>
2023-05-09 12:42:41 +0530

    TenX Trading UI

--- trading-app/src/layouts/tenXTrading/Header/index.js
+++ trading-app/src/layouts/tenXTrading/Header/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { CircularProgress, Grid } from '@mui/material';
 import MDBox from '../../../components/MDBox';
+import MDAvatar from '../../../components/MDAvatar';
 import MDTypography from '../../../components/MDTypography';
 import tradesicon from '../../../assets/images/tradesicon.png'
 
@@ -12,49 +13,57 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import Subs from '../../../assets/images/subs.png';
 
 const card = (
   <React.Fragment>
     <CardContent justifyContent="center" >
-      <img src="https://indianroyalmatrimony.com/admin_panel/admin/product_images/package_img/basic%20plan/basic-icon-png-500x500.png" style={{height:"30px"}}/>
-      <span style={{marginLeft:"13px",paddingTop:"45px"}}>hey</span>
-      <Typography sx={{ fontSize: 34 }} color="text.secondary" gutterBottom>
+      <MDBox>
+      <Grid container spacing={1} display="flex" justifyContent='flex-start' alignItems='center' alignContent='center'>
+        <Grid item xs={2} md={2} lg={2}>
+        <MDAvatar src={Subs} height={50} size="small" display="flex" justifyContent="left"/>
+        </Grid>
+        <Grid item xs={10} md={10} lg={10} display="flex" justifyContent='flex-start' alignItems='left'>
+        <MDTypography fontSize={20} fontWeight='bold'>Basic</MDTypography>
+        </Grid>
+      </Grid>
+      </MDBox>
+      <MDBox display='flex' justifyContent='center'>
+      <MDTypography sx={{ fontSize: 34 }} color="text.secondary" gutterBottom>
         ₹45
-        <span style={{fontSize:"13px"}}>/month</span>
-      <Typography variant="body2" color="text.secondary">
-        per user/month,billed anually
-       </Typography>
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="red">
+      </MDTypography>
+      <MDTypography style={{fontSize:"13px", lineHeight:5}}>/22 trading days</MDTypography>
+      </MDBox>
+      <MDTypography sx={{ mb: 1.5 }} color="red">
         Discount 
         <span> -25%</span>
-      </Typography>
-      <Typography variant="body2">
+      </MDTypography>
+      <MDTypography variant="body2">
        validity
-      </Typography>
-      <Typography gutterBottom variant="body2">
+      </MDTypography>
+      <MDTypography gutterBottom variant="body2">
        validity periods
-      </Typography>
-      <Typography  variant="body2">
+      </MDTypography>
+      <MDTypography  variant="body2">
        <CheckIcon sx={{color:"green"}}/>
        <span> plan1</span>
        
-      </Typography>
-      <Typography  variant="body2">
+      </MDTypography>
+      <MDTypography  variant="body2">
        <CheckIcon sx={{color:"green"}}/>
        <span> plan2</span>
        
-      </Typography>
-      <Typography  variant="body2">
+      </MDTypography>
+      <MDTypography  variant="body2">
        <CheckIcon sx={{color:"green"}}/>
        <span> plan3</span>
        
-      </Typography>
-      <Typography  variant="body2">
+      </MDTypography>
+      <MDTypography  variant="body2">
         <CloseIcon  sx={{color:"red"}}/>
        <span> plan4</span>
        
-      </Typography>
+      </MDTypography>
     </CardContent>
     
       {/* <button style={{background:"black",color:"white",borderRadius:"13px"}}>Purchase</button> */}
@@ -84,30 +93,30 @@ export default function LabTabs() {
 
 {/* <Box sx={{ minWidth: 275,display:"flex", flexWrap:"wrap",justifyContent:"center",alignItems:"center" }}> */}
         
-    <Grid container sx={{ml:"14px"}} rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3,lg:5 }} columns={{ xs: 2, sm: 8, md: 12 }} >
+    <Grid container spacing={3} >
 
-          <Grid item  xs={2} sm={4} md={4} >
-          <Card  style={{width:"85%"}} variant="outlined">{card}</Card>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Card variant="outlined">{card}</Card>
           </Grid>
 
-          <Grid item  xs={2} sm={4} md={4} >
-          <Card  style={{width:"85%"}} variant="outlined">{card}</Card>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Card variant="outlined">{card}</Card>
           </Grid>
 
-          <Grid item  xs={2} sm={4} md={4} >
-          <Card  style={{width:"85%"}} variant="outlined">{card}</Card>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Card variant="outlined">{card}</Card>
           </Grid>
 
-          <Grid item  xs={2} sm={4} md={4} >
-          <Card  style={{width:"85%"}} variant="outlined">{card}</Card>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Card variant="outlined">{card}</Card>
           </Grid>
 
-          <Grid item  xs={2} sm={4} md={4} >
-          <Card  style={{width:"85%"}} variant="outlined">{card}</Card>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Card variant="outlined">{card}</Card>
           </Grid>
 
-          <Grid item  xs={2} sm={4} md={4} >
-          <Card  style={{width:"85%"}} variant="outlined">{card}</Card>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Card variant="outlined">{card}</Card>
           </Grid>
 
           
