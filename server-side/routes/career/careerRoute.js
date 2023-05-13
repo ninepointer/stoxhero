38c6c82226c53539ff53d662c827e39f9bbe2660
someone <vvv201214@gmail.com>
2023-05-13 10:31:24 +0530

    career backend

--- server-side/routes/career/careerRoute.js
+++ server-side/routes/career/careerRoute.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const multer = require('multer');
 const aws = require('aws-sdk');
-const {getUploadsApplication, createCareer, getCareers, editCareer, getCareer, getCareerApplicantions} = require("../../controllers/careerController");
-const authentication = require("../../authentication/authentication")
+const {getUploadsApplication, createCareer, getCareers, editCareer, getCareer, getCareerApplicantions} = require("../../controllers/career/careerController");
+const authentication = require("../../authentication/authentication");
+const internBatchRoute = require("./internBatchRoute");
+const groupDiscussion = require("./groupDiscussionRoute");
+
 // createTenXSubscription
 
 aws.config.update({
@@ -11,7 +14,6 @@ aws.config.update({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: ""
 });
-
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -21,11 +23,11 @@ const upload = multer({
 });
 
 router.route('/').get(getCareers);
-router.route('/:id').get(getCareer);
 router.route('/userDetail').post(upload.array("files"), getUploadsApplication);
 router.route('/create').post(authentication, createCareer);
-router.route('/:id').patch(authentication, editCareer);
+router.route('/:id').get(getCareer).patch(authentication, editCareer);
 router.route('/careerapplications/:id').get(getCareerApplicantions);
-
+router.use('/:id/batch', internBatchRoute);
+router.use('/:id/groupDiscussion', groupDiscussion);
 
 module.exports = router;
