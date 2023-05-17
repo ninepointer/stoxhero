5cbf9bd8f4b66ca48edd9a8d18e5a108e9c70ced
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-05-17 16:48:47 +0530

    Internship College routes

---
+++ trading-app/src/layouts/college/index.js
@@ -0,0 +1,29 @@
+// Material Dashboard 2 React example components
+import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
+import Footer from "../../examples/Footer";
+import College from "./College";
+
+
+// Data
+// import authorsTableData from "./data/authorsTableData";
+// import projectsTableData from "./data/projectsTableData";
+import Header from "./Header";
+
+function Tables() {
+  // const { columns, rows } = authorsTableData();
+  // const { columns: pColumns, rows: pRows } = projectsTableData();
+
+  return (
+    <>
+    <DashboardLayout>
+      <DashboardNavbar />
+      {/* <Header/> */}
+      <College/>
+      <Footer />
+    </DashboardLayout>
+    </>
+  );
+}
+
+export default Tables;
