a446928e77ef76bc4b979678ecfd9a90a40f9ab5
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-05-20 01:59:16 +0530

    admin add to gd functionality

--- server-side/routes/career/groupDiscussionRoute.js
+++ server-side/routes/career/groupDiscussionRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const Authenticate = require('../../authentication/authentication');
 const router = express.Router({mergeParams: true});
-const {createGroupDiscussion, getBatchGroupDiscussion, getGroupDiscussion, getGroupDiscussions, editGroupDiscussion, approveUser, deleteGroupDiscussion, addUserToGd, selectCandidate} = require('../../controllers/career/groupDiscussion');
+const {createGroupDiscussion, getBatchGroupDiscussion, getGroupDiscussion, getGroupDiscussions, editGroupDiscussion, approveUser, deleteGroupDiscussion, addUserToGd, selectCandidate, getGdsByCareer} = require('../../controllers/career/groupDiscussion');
 
 
 router.route('/').post(Authenticate, createGroupDiscussion).get(getGroupDiscussions);
 router.route('/batch/:id').get(Authenticate, getBatchGroupDiscussion);
+router.route('/career/:careerId').get(getGdsByCareer);
 router.route('/add/:gdId/:userId').patch(Authenticate, addUserToGd);
 router.route('/select/:gdId/:userId').patch(Authenticate, selectCandidate);
 router.route('/:id').patch(Authenticate, editGroupDiscussion)
