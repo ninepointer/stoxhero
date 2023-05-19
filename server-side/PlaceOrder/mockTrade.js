0abfa6714053ef28ea6ddc2672f41130b5c42e49
someone <vvv201214@gmail.com>
2023-05-20 00:48:25 +0530

    queue

--- server-side/PlaceOrder/mockTrade.js
+++ server-side/PlaceOrder/mockTrade.js
@@ -86,15 +86,6 @@ exports.mockTrade = async (req, res) => {
     }
 
     function sellBrokerage(totalAmount){
-        // let brokerage = Number(brokerageDetailSell[0].brokerageCharge);
-        // let exchangeCharge = totalAmount * (Number(brokerageDetailSell[0].exchangeCharge) / 100);
-        // let gst = (brokerage + exchangeCharge) * (Number(brokerageDetailSell[0].gst) / 100);
-        // let sebiCharges = totalAmount * (Number(brokerageDetailSell[0].sebiCharge) / 100);
-        // let stampDuty = totalAmount * (Number(brokerageDetailSell[0].stampDuty) / 100);
-        // let sst = totalAmount * (Number(brokerageDetailSell[0].sst) / 100);
-        // let finalCharge = brokerage + exchangeCharge + gst + sebiCharges + stampDuty + sst;
-        // return finalCharge
-
         let brokerage = Number(brokerageDetailSell[0].brokerageCharge);
         let exchangeCharge = totalAmount * (Number(brokerageDetailSell[0].exchangeCharge) / 100);
         let sebiCharges = totalAmount * (Number(brokerageDetailSell[0].sebiCharge) / 100);
--- server-side/kafkaTest.js
+++ server-side/kafkaTest.js
@@ -1,69 +1,60 @@
 const tenx = require("./controllers/AutoTradeCut/autoTradeCut");
+const { Kafka } = require('kafkajs')
+const takeAutoTenxTrade = require("./controllers/AutoTradeCut/autoTrade");
 
 
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['b-1.democluster1.bagf1q.c3.kafka.ap-south-1.amazonaws.com:9092', 
+            'b-2.democluster1.bagf1q.c3.kafka.ap-south-1.amazonaws.com:9092', 
+            'b-3.democluster1.bagf1q.c3.kafka.ap-south-1.amazonaws.com:9092'],  // replace with your brokers
+})
 
-// const { Kafka } = require('kafkajs')
-
-// const kafka = new Kafka({
-//   clientId: 'my-app',
-//   brokers: ['b-1.democluster1.bagf1q.c3.kafka.ap-south-1.amazonaws.com:9092', 
-//             'b-2.democluster1.bagf1q.c3.kafka.ap-south-1.amazonaws.com:9092', 
-//             'b-3.democluster1.bagf1q.c3.kafka.ap-south-1.amazonaws.com:9092'],  // replace with your brokers
-// })
-
-// const createTopic = async (topicName) => {
-//     const admin = kafka.admin()
-//     await admin.connect()
+const createTopic = async (topicName) => {
+    const admin = kafka.admin()
+    await admin.connect()
   
-//     await admin.createTopics({
-//       topics: [{
-//         topic: topicName,
-//         numPartitions: 10,
-//         replicationFactor: 3
-//       }],
-//     })
+    await admin.createTopics({
+      topics: [{
+        topic: topicName,
+        numPartitions: 10,
+        replicationFactor: 3
+      }],
+    })
   
-//     await admin.disconnect()
-//   }
+    await admin.disconnect()
+}
   
-//   createTopic('my-topic');
-(async () => {
-    try {
-      console.log("hii");
-    //   console.log(await tenx());
-      let arr = await tenx();
-      console.log(arr);
-    } catch (error) {
-      console.error(error);
-    }
-  })();
-// (async () => {
-//   const producer = kafka.producer()
+
+async function test(){
+  console.log("is function", typeof(takeAutoTenxTrade))
+  createTopic('my-topic');
+
+  let arr = await tenx();
+
+  const producer = kafka.producer()
   
-//   await producer.connect()
-//   await producer.send({
-//     topic: 'my-topic',
-//     messages: arr,
-//   })
+  await producer.connect()
+  await producer.send({
+    topic: 'my-topic',
+    messages: arr,
+  })
 
-//   await producer.disconnect()
+  await producer.disconnect()
 
-//   const consumer = kafka.consumer({ groupId: 'my-group' })
+  const consumer = kafka.consumer({ groupId: 'my-group' })
 
-//   await consumer.connect()
-//   await consumer.subscribe({ topic: 'my-topic', fromBeginning: true })
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'my-topic', fromBeginning: true })
 
-//   await consumer.run({
-//     eachMessage: async ({ topic, partition, message }) => {
-//       console.log({
-//         value: message.value.toString(),
-//       })
-//     },
-//   })
-// })().catch(console.error)
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      // console.log({
+      //   value: message.value.toString(),
+      // })
+      await takeAutoTenxTrade(message.value.toString());
+    },
+  })
+}
 
-
-// const tenx = require("./controllers/AutoTradeCut/autoTradeCut");
-// let arr = await tenx();
-// console.log(arr)
-// 
+module.exports = test;
