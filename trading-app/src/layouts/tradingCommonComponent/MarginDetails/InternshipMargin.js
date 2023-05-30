5b0ba9cb139d7986c0ff48d4a202a6fde88f9702
someone <vvv201214@gmail.com>
2023-05-31 01:07:14 +0530

    intern bug fix

--- trading-app/src/layouts/tradingCommonComponent/MarginDetails/InternshipMargin.js
+++ trading-app/src/layouts/tradingCommonComponent/MarginDetails/InternshipMargin.js
@@ -65,7 +65,7 @@ const InternShipMargin = ({BatchId, setyesterdayData}) => {
                 <Grid item xs={16} md={6} xl={3}>
                   <DefaultInfoCard
                     // icon={<CreditCardIcon/>}
-                    title="total credit"
+                    title="Portfolio Value"
                     description="Total funds added by StoxHero in your Account"
                     value={totalCreditString}
                   />
