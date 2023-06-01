32b0b69e820c33b4c3d209a59d557c55edad9bff
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-06-01 19:42:44 +0530

    Minor changes and fixes

--- trading-app/src/layouts/infinitydashboard/Header/index.js
+++ trading-app/src/layouts/infinitydashboard/Header/index.js
@@ -673,7 +673,7 @@ export default function LabTabs({socket}) {
 
                                   
                             >
-                                SroxHero Orders
+                                StoxHero Orders
                             </MDButton>
                         </Grid>
 
