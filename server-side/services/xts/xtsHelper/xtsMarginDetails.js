2ce8ea14890d77bd176f99c787902b9699da5c3a
someone <vvv201214@gmail.com>
2023-06-04 10:01:35 +0530

    test

--- server-side/services/xts/xtsHelper/xtsMarginDetails.js
+++ server-side/services/xts/xtsHelper/xtsMarginDetails.js
@@ -4,9 +4,76 @@ require("../../../db/conn");
 const RequestToken = require("../../../models/Trading Account/requestTokenSchema");
 const axios = require("axios");
 const {xtsAccountType} = require("../../../constant");
+const InfinityLiveCompany = require("../../../models/TradeDetails/liveTradeSchema")
 
 
 router.get("/xtsMargin", async (req, res)=>{
+  let pnlLiveDetails = await InfinityLiveCompany.aggregate([
+    {
+      $lookup: {
+        from: 'algo-tradings',
+        localField: 'algoBox',
+        foreignField: '_id',
+        as: 'algo'
+      }
+    },
+    {
+      $match: {
+        trade_time: {
+          $gte: today
+        },
+        status: "COMPLETE",
+        "algo.isDefault": true
+      },
+    },
+    {
+      $group: {
+        _id: {
+          symbol: "$symbol",
+          product: "$Product",
+          instrumentToken: "$instrumentToken",
+          exchangeInstrumentToken: "$exchangeInstrumentToken",
+        },
+        amount: {
+          $sum: { $multiply: ["$amount", -1] },
+        },
+        brokerage: {
+          $sum: {
+            $toDouble: "$brokerage",
+          },
+        },
+        lots: {
+          $sum: {
+            $toInt: "$Quantity",
+          },
+        },
+        lastaverageprice: {
+          $last: "$average_price",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        symbol: "$_id.symbol",
+        product: "$_id.product",
+        amount: 1,
+        brokerage: 1,
+        instrumentToken: "$_id.instrumentToken",
+        exchangeInstrumentToken: "$_id.exchangeInstrumentToken",
+        npnl: {
+          $subtract: ["$amount", "$brokerage"]
+        },
+        lots: 1,
+        lastaverageprice: 1
+      },
+    },
+    {
+      $sort: {
+        _id: -1,
+      },
+    },
+  ])
     const accessToken = await RequestToken.find({status: "Active", accountType: xtsAccountType, xtsType: "Interactive"});
 
     let url = `${process.env.INTERACTIVE_URL}/interactive/user/balance?clientID=${process.env.XTS_CLIENTID}`;
