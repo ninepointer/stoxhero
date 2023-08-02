252ec25046c598f6bfde346a63a4e7db2ba8a72e
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-08-02 11:01:41 +0530

    payment status page fix

--- trading-app/src/layouts/paymentTest/paymentStatus.js
+++ trading-app/src/layouts/paymentTest/paymentStatus.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MDBox from '../../components/MDBox';
 import { useLocation, useNavigate } from 'react-router-dom';
 import MDTypography from '../../components/MDTypography';
 import { apiUrl } from '../../constants/constants';
+import axios from 'axios';
 
 const PaymentStatus = () => {
     const location = useLocation();
