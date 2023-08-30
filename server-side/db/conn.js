5a77e4416ac5fdfc8aee600bd4f1d70be76be40c
Vijay <vvv201214@gmail.com>
2023-08-30 12:46:06 +0530

    contest master

--- server-side/db/conn.js
+++ server-side/db/conn.js
@@ -10,10 +10,10 @@ const stagingDB = process.env.STAGINGDB;
 const infinityDB = process.env.INFINITYDB;
 
 
-    // mongoose.connect(devDB, {
+    mongoose.connect(devDB, {
         //  mongoose.connect(DB, {
         // mongoose.connect(stagingDB, {
-        mongoose.connect(infinityDB, {
+        // mongoose.connect(infinityDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useFindAndModify: false
