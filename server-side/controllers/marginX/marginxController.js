7493a7e4ab046c167fcb7ce81421bc9c69228f00
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-09-02 19:16:21 +0530

    added controllers for payout and participate

--- server-side/controllers/marginX/marginxController.js
+++ server-side/controllers/marginX/marginxController.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const MarginX = require('../../models/marginX/marginX'); // Assuming your model is exported as Contest from the mentioned path
+const MarginX = require('../../models/marginX/marginX'); 
+const Wallet  = require('../../models/UserWallet/userWalletSchema');
+const MarginXMockUser = require('../../models/marginX/marginXTrade');
 // const { ObjectId } = require('mongodb');
 // const uuid = require("uuid");
 // const emailService = require("../../utils/emailService");
@@ -248,3 +250,224 @@ exports.getDraftMarginXs = async (req,res,next) => {
         });
     }
 }
+
+exports.creditAmountToWallet = async () => {
+    try {
+        let date = new Date();
+        let todayDate = `${(date.getFullYear())}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
+        todayDate = todayDate + "T00:00:00.000Z";
+        const today = new Date(todayDate);
+
+        const marginxs = await MarginX.find({ status: "Completed", payoutStatus: null, endTime: {$gte: today} }).populate('marginXTemplate', 'portfolioValue entryFee');
+
+        // console.log(contest.length, contest)
+        for (let j = 0; j < marginxs.length; j++) {
+            // if (contest[j].contestEndTime < new Date()) {
+            let leverage = marginxs[j]?.marginXTemplate?.portfolioValue/marginxs[j]?.entryFee;
+            let entryFee = marginxs[j]?.entryFee;
+            for (let i = 0; i < marginxs[j]?.participants?.length; i++) {
+                let userId = marginxs[j]?.participants[i]?.userId;
+                let id = marginxs[j]._id;
+                let pnlDetails = await MarginXMockUser.aggregate([
+                    {
+                        $match: {
+                            trade_time: {
+                                $gte: today
+                            },
+                            status: "COMPLETE",
+                            trader: new ObjectId(userId),
+                            marginx: new ObjectId(id)
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: {
+                            },
+                            amount: {
+                                $sum: {
+                                    $multiply: ["$amount", -1],
+                                },
+                            },
+                            brokerage: {
+                                $sum: {
+                                    $toDouble: "$brokerage",
+                                },
+                            },
+                        },
+                    },
+                    {
+                        $project:
+                        {
+                            npnl: {
+                                $subtract: ["$amount", "$brokerage"],
+                            },
+                        },
+                    },
+                ])
+
+                // console.log(pnlDetails[0]);
+                const payoutAmount = pnlDetails[0]?.npnl/leverage + entryFee;
+                if(payoutAmount >0){
+                    const wallet = await Wallet.findOne({ userId: userId });
+                    console.log(userId, pnlDetails[0]);
+
+                    wallet.transactions = [...wallet.transactions, {
+                        title: 'Marginx Credit',
+                        description: `Amount credited for Marginx ${marginxs[j].name}`,
+                        transactionDate: new Date(),
+                        amount: payoutAmount?.toFixed(2),
+                        transactionId: uuid.v4(),
+                        transactionType: 'Cash'
+                    }];
+                    await wallet.save();
+
+                    marginxs[j].participants[i].payout = payoutAmount?.toFixed(2);
+                    await marginxs[j].save();
+                }
+            }
+            marginxs[j].payoutStatus = 'Completed'
+            marginxs[j].status = "Completed";
+            await marginxs[j].save();
+            // }
+        }
+
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.purchaseIntent = async (req, res) => {
+    try {
+        const { id } = req.params; // ID of the contest 
+        const userId = req.user._id;
+
+        const result = await MarginX.findByIdAndUpdate(
+            id,
+            { $push: { purchaseIntent: { userId: userId, date: new Date() } } },
+            { new: true }  // This option ensures the updated document is returned
+        );
+
+        if (!result) {
+            return res.status(404).json({ status: "error", message: "Something went wrong." });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Intent Saved successfully",
+            data: result
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+};
+
+exports.copyAndShare = async (req, res) => {
+    try {
+        const { id } = req.params; // ID of the contest and the user to register
+        const userId = req.user._id;
+
+        if (!mongoose.Types.ObjectId.isValid(id) || !mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({ status: "error", message: "Invalid contest ID or user ID" });
+        }
+
+        const result = await MarginX.findByIdAndUpdate(
+            id,
+            {
+                $addToSet: {
+                    contestSharedBy: {
+                        $each: [
+                            {
+                                userId: userId,
+                                sharedAt: new Date(),
+                            },
+                        ],
+                    },
+                },
+            },
+            { new: true }
+        );
+
+        if (!result) {
+            return res.status(404).json({ status: "error", message: "Contest not found" });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "User share to contest successfully",
+            data: result
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+};
+
+exports.participateUsers = async (req, res) => {
+    try {
+        const { id } = req.params; // ID of the contest 
+        const userId = req.user._id;
+
+        const marginx = await MarginX.findOne({ _id: id });
+
+        for (let i = 0; i < marginx.participants?.length; i++) {
+            if (marginx.participants[i]?.userId?.toString() === userId?.toString()) {
+                return res.status(404).json({ status: "error", message: "You have already participated in this marginx." });
+            }
+        }
+
+        const getActiveMarginX = await MarginX.find({
+            participants: {
+                $elemMatch: {
+                    userId: new ObjectId(userId)
+                }
+            },
+            contestStatus: "Active",
+            // entryFee: 0,
+            $or: [
+                { startTime: { $gte: new Date(marginx.startTime), $lte: new Date(marginx.endTime) } },
+                { endTime: { $gte: new Date(marginx.startTime), $lte: new Date(marginx.endTime) } },
+                {
+                    $and: [
+                        { contestStartTime: { $lte: new Date(marginx.startTime) } },
+                        { contestEndTime: { $gte: new Date(marginx.endTime) } }
+                    ]
+                }
+            ]
+        })
+
+        if (getActiveMarginX.length > 0) {
+            if (!marginx.potentialParticipants.includes(userId)) {
+                marginx.potentialParticipants.push(userId);
+                await marginx.save();
+            }
+            return res.status(404).json({ status: "error", message: "You can only participate in another marginx once your current marginx ends!" });
+        }
+
+        if (marginx?.maxParticipants <= marginx?.participants?.length) {
+            if (!marginx.potentialParticipants.includes(userId)) {
+                marginx.potentialParticipants.push(userId);
+                await marginx.save();
+            }
+            return res.status(404).json({ status: "error", message: "The marginx is already full. We sincerely appreciate your enthusiasm to participate in the marginX. Please join in the next marginX." });
+        }
+        res.status(200).json({
+            status: "success",
+            message: "Participate successfully",
+            data: result
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+};
