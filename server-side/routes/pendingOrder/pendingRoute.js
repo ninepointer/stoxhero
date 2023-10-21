fd0a8d7c66cf119934ff3d26eff2e00020f8fc5c
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-10-21 13:04:35 +0530

    wip

--- server-side/routes/pendingOrder/pendingRoute.js
+++ server-side/routes/pendingOrder/pendingRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
-const {myTodaysPendingTrade, myTodaysProcessedTrade, cancelOrder, modifyOrder} = require('../../controllers/PendingOrderController');
+const {myTodaysPendingTrade, myTodaysProcessedTrade, cancelOrder, modifyOrder, editPrice} = require('../../controllers/PendingOrderController');
 const Authenticate = require('../../authentication/authentication');
 
 
