82286765dfb617d7f31d5773e5a623fed1fc28cd
vijay <vvv201214@gmail.com>
2023-10-25 11:53:14 +0530

    stoploss

--- server-side/textEncoding.js
+++ server-side/textEncoding.js
@@ -15,14 +15,4 @@ const textDecoder = new TextDecoder();
 // Decode the binary data back to the original text
 const decodedData = textDecoder.decode(binaryData);
 
-console.log(decodedData); 
-
-// curl https://api.kite.trade/session/token \
-//    -H "X-Kite-Version: 3" \
-//    -d "api_key=nq0gipdzk0yexyko" \
-//    -d "request_token=AKEqoU5PXhQZM9D8dddgf2M34auGlCiv" \
-//    -d "checksum=172bbc6561201b5ebe760a73637cc1874686352e0435a2254424bd7c452609b3"
-
-
-// NR0563
-// Tlkndk@11
+console.log(decodedData);
