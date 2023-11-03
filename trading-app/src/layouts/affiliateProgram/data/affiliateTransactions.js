8ef767cdbe1067191419ec94731a8d951c0b3e9d
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-11-04 01:02:06 +0530

    downloads in grids added

--- trading-app/src/layouts/affiliateProgram/data/affiliateTransactions.js
+++ trading-app/src/layouts/affiliateProgram/data/affiliateTransactions.js
@@ -7,6 +7,10 @@ import MDTypography from "../../../components/MDTypography"
 import Card from "@mui/material/Card";
 import { apiUrl } from '../../../constants/constants';
 import moment from 'moment';
+import { saveAs } from 'file-saver';
+import { Tooltip } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
+
 
 
 export default function AffiliateGrid({data}) {
@@ -124,17 +128,74 @@ export default function AffiliateGrid({data}) {
     );
 
     rows.push(featureObj)
-  })
+  });
+  const handleDownload = (csvData, nameVariable) => {
+    // Create the CSV content
+    // const csvContent = csvData.map(row => row.join(',')).join('\n');
+    const csvContent = csvData?.map((row) => {
+      return row?.map((row1) => row1.join(',')).join('\n');
+    });
+    // const csvContent = 'Date,Weekday,Gross P&L(S) Gross P&L(I) Net P&L(S) Net P&L(I) Net P&L Diff(S-I)\nValue 1,Value 2,Value 3\nValue 4, Value 5, Value 6';
+
+    // Create a Blob object with the CSV content
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8' });
+
+    // Save the file using FileSaver.js
+    saveAs(blob, `${nameVariable}.csv`);
+  }
+
+  function downloadHelper(data) {
+    let csvDataFile = [[]]
+    let csvDataDailyPnl = [["INDEX", "NAME", "MOBILE", "CODE", "BUYER", "BUYER MOBILE", "BOUGHT AT", "PRODUCT", "PURCHASE VALUE", "NET PURCHASE VALUE", "COMMISSION"]]
+    if (data) {
+      // dates = Object.keys(data)
+      let csvpnlData = Object.values(data)
+      csvDataFile = csvpnlData?.map((elem, index) => {
+
+        return [
+          index+1,
+          `${elem?.affiliate?.first_name} ${elem?.affiliate?.last_name}`,
+          elem?.affiliate?.mobile,
+          elem?.affiliate?.myReferralCode,
+          `${elem?.buyer?.first_name} ${elem?.buyer?.last_name}`,
+          elem?.buyer?.mobile,
+          moment(elem?.transactionDate).format('DD-MM-YY HH:mm:ss a'),
+          elem?.product?.productName,
+          elem?.productActualPrice,
+          elem?.productDiscountedPrice,
+          elem?.affiliatePayout,
+        ]
+      })
+    }
+
+    return [[...csvDataDailyPnl, ...csvDataFile]]
+  }
+
+  const pnlData = downloadHelper(newData);
+
 
   return (
     <Card>
-      <MDBox display="flex" justifyContent="space-between" alignItems="left">
+      {/* <MDBox display="flex" justifyContent="space-between" alignItems="left">
         <MDBox width="100%" display="flex" justifyContent="center" alignItems="center" sx={{backgroundColor:"lightgrey",borderRadius:"2px"}}>
           <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
             Affiliate Transactions({newData?.length ? newData?.length : 0})
           </MDTypography>
         </MDBox>
+      </MDBox> */}
+      <MDBox display="flex" justifyContent="space-between" alignItems="left">
+        <MDBox width="100%" display="flex" justifyContent="space-between" alignItems="center" sx={{ backgroundColor: "lightgrey", borderRadius: "2px" }}>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
+          </MDTypography>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
+          Affiliate Transactions({newData?.length ? newData?.length : 0})
+          </MDTypography>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} gutterBottom >
+            <Tooltip title="Download CSV"><MDBox sx={{ backgroundColor: "lightgrey", borderRadius: "2px", cursor: "pointer", marginRight: "5px" }} onClick={() => { handleDownload(pnlData, `affiliate-transactions`) }}><DownloadIcon /></MDBox></Tooltip>
+          </MDTypography>
+        </MDBox>
       </MDBox>
+
       <MDBox mt={1}>
         <DataTable
           table={{ columns, rows }}
--- trading-app/src/layouts/career/data/rejectedApplicant.js
+++ trading-app/src/layouts/career/data/rejectedApplicant.js
@@ -7,6 +7,10 @@ import MDTypography from "../../../components/MDTypography"
 import Card from "@mui/material/Card";
 import axios from "axios";
 import AddApplicantModal from '../AddApplicantModal';
+import { saveAs } from 'file-saver';
+import { Tooltip } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
+import moment from 'moment';
 
 
 export default function Applicants({career, action, setAction}) {
@@ -125,13 +129,70 @@ export default function Applicants({career, action, setAction}) {
 
   rows.push(featureObj)
 })
+const handleDownload = (csvData, nameVariable) => {
+  // Create the CSV content
+  // const csvContent = csvData.map(row => row.join(',')).join('\n');
+  const csvContent = csvData?.map((row) => {
+    return row?.map((row1) => row1.join(',')).join('\n');
+  });
+  // const csvContent = 'Date,Weekday,Gross P&L(S) Gross P&L(I) Net P&L(S) Net P&L(I) Net P&L Diff(S-I)\nValue 1,Value 2,Value 3\nValue 4, Value 5, Value 6';
+
+  // Create a Blob object with the CSV content
+  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8' });
+
+  // Save the file using FileSaver.js
+  saveAs(blob, `${nameVariable}.csv`);
+}
+
+function downloadHelper(data) {
+  let csvDataFile = [[]]
+  let csvDataDailyPnl = [["NAME", "DOB", "EMAIL","MOBILE", "IN ACTIVE BATCH", "COMPLETED", "PRIOR EXP", "SOURCE", "STATUS", "LINKEDIN", "APPLIED ON", "COLLEGE NAME"]]
+  if (data) {
+    // dates = Object.keys(data)
+    let csvpnlData = Object.values(data)
+    csvDataFile = csvpnlData?.map((elem) => {
+
+      return [
+        `${elem?.first_name} ${elem?.last_name}`,
+        moment(elem?.dob).format('DD-MM-YY HH:mm:ss a'),
+        elem?.email,
+        elem?.mobileNo,
+        elem?.inActiveBatch,
+        elem?.completed,
+        elem?.priorTradingExperience,
+        elem?.source,
+        elem?.applicationStatus,
+        elem?.linkedInProfileLink,
+        moment(elem?.appliedOn).format('DD-MM-YY HH:mm:ss a'),
+        elem?.collegeName,
+      ]
+    })
+  }
+
+  return [[...csvDataDailyPnl, ...csvDataFile]]
+}
+
+const pnlData = downloadHelper(careerApplications)
+
 
   return (
     <Card>
-      <MDBox display="flex" justifyContent="space-between" alignItems="left">
+      {/* <MDBox display="flex" justifyContent="space-between" alignItems="left">
         <MDBox width="100%" display="flex" justifyContent="center" alignItems="center" sx={{backgroundColor:"lightgrey",borderRadius:"2px"}}>
           <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
             Rejected Applications({applicationCount})
+          </MDTypography>
+        </MDBox>
+      </MDBox> */}
+       <MDBox display="flex" justifyContent="space-between" alignItems="left">
+        <MDBox width="100%" display="flex" justifyContent="space-between" alignItems="center" sx={{ backgroundColor: "lightgrey", borderRadius: "2px" }}>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
+          </MDTypography>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} alignItems="center" gutterBottom>
+            Rejected Applications({applicationCount})
+          </MDTypography>
+          <MDTypography variant="text" fontSize={12} color="black" mt={0.7} gutterBottom >
+            <Tooltip title="Download CSV"><MDBox sx={{ backgroundColor: "lightgrey", borderRadius: "2px", cursor: "pointer", marginRight: "5px" }} onClick={() => { handleDownload(pnlData, `rejected-applicants`) }}><DownloadIcon /></MDBox></Tooltip>
           </MDTypography>
         </MDBox>
       </MDBox>
