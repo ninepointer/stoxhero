58dd633a339ae6c6e47cc3e54ded4e55e1657a2c
vijay <vvv201214@gmail.com>
2023-11-11 13:34:44 +0530

    muhurat

--- server-side/controllers/userWalletController.js
+++ server-side/controllers/userWalletController.js
@@ -74,7 +74,15 @@ exports.myWallet = async (req, res, next) => {
             { $match: { "userId": mongoose.Types.ObjectId(userId) } },
 
             // Unwinding the transactions to sort them
-            { $unwind: "$transactions" },
+            { $unwind: 
+                
+                {
+                    $unwind: {
+                      path: "$transactions",
+                      preserveNullAndEmptyArrays: true
+                    }
+                  },
+            },
 
             // Sorting transactions by transactionDate in descending order
             { $sort: { "transactions.transactionDate": -1 } },
