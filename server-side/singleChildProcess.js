36c254f2277188bc608c6fba9245104d6054768f
vijay <vvv201214@gmail.com>
2023-11-24 20:03:51 +0530

    blog

--- server-side/singleChildProcess.js
+++ server-side/singleChildProcess.js
@@ -414,10 +414,8 @@ async function singleProcess() {
 
 
     const PORT = process.env.PORT || 5002;
-    // const server = app.listen(PORT);
-    const server = app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Server is running on port ${PORT}`);
-      });
+    const server = app.listen(PORT);
+
 
     if(process.env.CHART === "true"){
         webSocketService.init(io);
