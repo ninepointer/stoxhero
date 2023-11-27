a785e519c8898ba031881342b1328ebcfd8fabeb
vijay <vvv201214@gmail.com>
2023-11-27 23:28:38 +0530

    blog

--- trading-app/src/layouts/HomePage/pages/BlogCards.jsx
+++ trading-app/src/layouts/HomePage/pages/BlogCards.jsx
@@ -113,7 +113,7 @@ export default function BlogCard() {
                                         <CardActionArea>
                                           <Grid item xs={12} md={4} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
                                             <img src={elem?.thumbnailImage?.url} style={{ maxWidth: '100%', height: 'auto', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} />
-                                            <Typography variant="h6" fontSize={10} fontFamily='Segoe UI' fontWeight={400} style={{ position: 'absolute', top: 0, right: 0, margin: '8px', textAlign: 'center', color: '#A4A7B0', backgroundColor: "white", borderRadius: "15px", padding: "2px 10px 2px 10px", marginTop: "10px" }}>
+                                            <Typography variant="h6" fontSize={10} fontFamily='Segoe UI' fontWeight={400} style={{ position: 'absolute', top: 0, right: 0, margin: '8px', textAlign: 'center', color: 'black', backgroundColor: "white", borderRadius: "15px", padding: "2px 10px 2px 10px", marginTop: "10px" }}>
                                               {elem?.category?.toUpperCase() || "F&O"}
                                             </Typography>
                                           </Grid>
