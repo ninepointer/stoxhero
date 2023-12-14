2bf540f66dfe43f349ebd80e67a1221d26bf5347
vijay <vvv201214@gmail.com>
2023-12-14 09:47:46 +0530

    affiliate

--- server-side/routes/HistoryPages/adminAuth.js
+++ server-side/routes/HistoryPages/adminAuth.js
@@ -3114,7 +3114,7 @@ router.get("/updateRole", async (req, res) => {
 
 router.get("/updateInstrumentStatus", async (req, res) => {
   let date = new Date();
-  let expiryDate = "2023-12-12T20:00:00.000+00:00"
+  let expiryDate = "2023-12-13T20:00:00.000+00:00"
   expiryDate = new Date(expiryDate);
 
   let instrument = await Instrument.updateMany(
