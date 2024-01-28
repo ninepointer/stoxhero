59f437ae7791922d23688d38134b6c37ebd42f4e
vijay <vvv201214@gmail.com>
2024-01-28 21:25:45 +0530

    kyc

--- server-side/controllers/dailyContestTradeController.js
+++ server-side/controllers/dailyContestTradeController.js
@@ -2307,8 +2307,8 @@ async function processContestQueue() {
 
     const endTime = new Date(currentTime);
     endTime.setHours(9, 48, 0, 0);
-    //todo-vijay
-    // if (currentTime >= startTime && currentTime <= endTime) {
+
+    if (currentTime >= startTime && currentTime <= endTime) {
 
         // If the queue is empty, reset the processing flag and return
         if (contestQueue.length === 0) {
@@ -2327,7 +2327,7 @@ async function processContestQueue() {
             }
         }
 
-    // }
+    }
 }
 
 exports.sendMyRankData = async () => {
@@ -2344,8 +2344,8 @@ exports.sendMyRankData = async () => {
                 startTime.setHours(3, 0, 0, 0);
                 const endTime = new Date(currentTime);
                 endTime.setHours(9, 48, 0, 0);
-                //todo-vijay
-                // if (currentTime >= startTime && currentTime <= endTime) {
+
+                if (currentTime >= startTime && currentTime <= endTime) {
                     const contest = await DailyContest.find({ contestStatus: "Active", contestStartTime: { $lte: new Date() } });
 
                     for (let i = 0; i < contest?.length; i++) {
@@ -2366,7 +2366,7 @@ exports.sendMyRankData = async () => {
                             }
                         }
                     }
-                // }
+                }
             };
             emitLeaderboardData();
             interval = setInterval(emitLeaderboardData, 5000);
