ab1236f8e8aaa603f6961747518f6b112d02657b
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2024-02-14 00:57:13 +0530

    leaderboard limit

--- server-side/controllers/dailyContestTradeController.js
+++ server-side/controllers/dailyContestTradeController.js
@@ -2297,7 +2297,7 @@ async function processContestQueue() {
     endTime.setHours(9, 48, 0, 0);
 
     //todo-vijay
-  //  if (currentTime >= startTime && currentTime <= endTime) {
+   if (currentTime >= startTime && currentTime <= endTime) {
 
         // If the queue is empty, reset the processing flag and return
         if (contestQueue.length === 0) {
@@ -2316,7 +2316,7 @@ async function processContestQueue() {
             }
         }
 
-  //  }
+   }
 }
 
 exports.sendMyRankData = async () => {
@@ -2335,7 +2335,7 @@ exports.sendMyRankData = async () => {
                 endTime.setHours(9, 48, 0, 0);
 
                 //todo-vijay
-               // if (currentTime >= startTime && currentTime <= endTime) {
+               if (currentTime >= startTime && currentTime <= endTime) {
                     const contest = await DailyContest.find({ contestStatus: "Active", contestStartTime: { $lte: new Date() } });
 
                     for (let i = 0; i < contest?.length; i++) {
@@ -2356,7 +2356,7 @@ exports.sendMyRankData = async () => {
                             }
                         }
                     }
-                //}
+                }
             };
             emitLeaderboardData();
             interval = setInterval(emitLeaderboardData, 5000);
