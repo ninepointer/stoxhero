e9804e801ca52e50f2d6455149c0fb0407e82785
vijay <vvv201214@gmail.com>
2024-02-15 21:19:17 +0530

    school

--- server-side/authentication/authorization.js
+++ server-side/authentication/authorization.js
@@ -4,14 +4,12 @@ const Role = require('../models/User/everyoneRoleSchema');
 const hasRole = (...requiredRoles) => {
     return async (req, res, next) => {
         try {
-            console.log(requiredRoles, req.user.role)
             // assuming the user object has a role field containing the roleId
             const role = await Role.findById(req.user.role);
             
             if (!role) {
                 return res.status(403).json({ status: 'error', message: 'Role does not exist' });
             }
-            console.log(role.roleName)
             if (!requiredRoles.includes(role.roleName)) {
                 return res.status(403).json({ status:'error',message: 'Access Denied: Insufficient Permissions' });
             }
