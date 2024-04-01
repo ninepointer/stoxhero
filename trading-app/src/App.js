59bd1373a2bdbc99c399e065c93768282f619f74
vijay <vvv201214@gmail.com>
2024-04-01 09:38:21 +0530

    bug fixes

--- trading-app/src/App.js
+++ trading-app/src/App.js
@@ -97,7 +97,7 @@ import homeRoutes from "./homeRoute";
 // import Privacy from "./layouts/HomePage/pages/Privacy";
 // import Terms from "./layouts/HomePage/pages/Tnc";
 
-// import MessagePopUp from "./MessagePopup";
+import MessagePopUp from "./MessagePopup";
 
 // import AdminLogin from "./layouts/authentication/sign-in/adminLogin";
 // import SchoolLogin from "./layouts/authentication/sign-in/schoolLogin";
@@ -132,9 +132,6 @@ const ResetPassword = lazy(() => import("./layouts/authentication/reset-password
 const Contact = lazy(() => import("./layouts/HomePage/pages/Contact"));
 const Privacy = lazy(() => import("./layouts/HomePage/pages/Privacy"));
 const Terms = lazy(() => import("./layouts/HomePage/pages/Tnc"));
-
-const MessagePopUp = lazy(() => import("./MessagePopup"));
-
 const AdminLogin = lazy(() => import("./layouts/authentication/sign-in/adminLogin"));
 const SchoolLogin = lazy(() => import("./layouts/authentication/sign-in/schoolLogin"));
 const TradingGuru = lazy(() => import("./layouts/authentication/sign-up/tradingguru"));
