960d23b93f1f9a1662ea925223d15e9440a3e54e
vijay <vvv201214@gmail.com>
2024-04-10 12:08:11 +0530

    tenx

--- server-side/controllers/tenXSubscriptionController.js
+++ server-side/controllers/tenXSubscriptionController.js
@@ -663,13 +663,13 @@ exports.handleSubscriptionRenewal = async (userId, subscriptionAmount, subscript
       }
     }
 
-    const newBonusAmount = discountAmount + bonusRedemption;
+    const newBonusAmount = Number((discountAmount)?.toFixed(2)) + Number((bonusRedemption)?.toFixed(2));
     const totalAmount = (tenXSubs?.discounted_price - newBonusAmount) * (1 + setting[0]?.gstPercentage / 100)
 
-    console.log(Number(totalAmount), Number(subscriptionAmount));
-    console.log(tenXSubs?.discounted_price, discountAmount, bonusRedemption, (1 + setting[0]?.gstPercentage / 100));
+    console.log(Number(totalAmount), Number((subscriptionAmount)?.toFixed(2)));
+    console.log(tenXSubs?.discounted_price, Number((discountAmount)?.toFixed(2)) , Number((bonusRedemption)?.toFixed(2)), (1 + setting[0]?.gstPercentage / 100));
 
-    if (Number(totalAmount) != Number(subscriptionAmount)) {
+    if (Number(totalAmount) != Number((subscriptionAmount)?.toFixed(2))) {
       return {
         statusCode: 400,
         data: {
--- server-side/controllers/userWalletController.js
+++ server-side/controllers/userWalletController.js
@@ -246,11 +246,11 @@ exports.handleDeductSubscriptionAmount = async(userId, subscriptionAmount, subsc
             }
         }
 
-        const newBonusAmount = discountAmount + bonusRedemption;
+        const newBonusAmount = Number((discountAmount)?.toFixed(2)) + Number((bonusRedemption)?.toFixed(2));
         const totalAmount = (subs?.discounted_price - newBonusAmount)*(1+setting[0]?.gstPercentage/100)
-        console.log(Number(totalAmount) , Number(subscriptionAmount));
-        console.log(subs?.discounted_price, discountAmount, bonusRedemption, (1 + setting[0]?.gstPercentage / 100));
-        if(Number(totalAmount) != Number(subscriptionAmount)){
+        console.log(Number(totalAmount) , Number((subscriptionAmount)?.toFixed(2)));
+        console.log(subs?.discounted_price,  Number((discountAmount)?.toFixed(2)) , Number((bonusRedemption)?.toFixed(2)), (1 + setting[0]?.gstPercentage / 100));
+        if(Number(totalAmount) != Number((subscriptionAmount)?.toFixed(2))){
             result= {
             statusCode:400,
             data:{
