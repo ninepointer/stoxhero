b11e6b1d1cad3a2cb79363f59fe1aa8feeeb5c2c
Vijay Verma <101820333+vvv201214@users.noreply.github.com>
2024-05-13 07:00:10 +0530

    Update singleChildProcess.js

--- server-side/singleChildProcess.js
+++ server-side/singleChildProcess.js
@@ -142,7 +142,16 @@ async function singleProcess() {
 
     app.use(express.json({ limit: "10mb" }));
     app.use(require("cookie-parser")());
-    const allowedOrigins = ['http://localhost:3000', 'https://stoxhero.com', 'https://www.stoxhero.com', 'https://stoxhero-next-ts.vercel.app', 'https://www.stoxhero-next-ts.vercel.app', 'http://43.204.7.180'];
+    const allowedOrigins = [
+    "http://localhost:3000",
+    "https://staging.stoxhero.com",
+    "http://staging.stoxhero.com",
+    "https://stoxhero.com",
+    "https://www.stoxhero.com",
+    "https://stoxhero-next-ts.vercel.app",
+    "https://www.stoxhero-next-ts.vercel.app",
+    "http://43.204.7.180",
+  ];
 
     const corsOptions = {
       credentials: true,
