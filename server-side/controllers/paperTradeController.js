3151618596a22c0f2613523068094a70edc4cdc3
vijay <vvv201214@gmail.com>
2024-05-24 08:36:08 +0530

    leaderboard fix

--- server-side/controllers/paperTradeController.js
+++ server-side/controllers/paperTradeController.js
@@ -1408,11 +1408,11 @@ exports.weeklyLeaderboardData = async (req, res) => {
 exports.monthlyLeaderboardData = async (req, res) => {
   try {
     const today = moment();
-    const newStartOfMonth = moment('2024-05-14').startOf('day').subtract(5, 'hours').subtract(30, 'minutes');
+    // const newStartOfMonth = moment('2024-05-14').startOf('day').subtract(5, 'hours').subtract(30, 'minutes');
 
     const startOfMonth = today.clone().startOf('month').subtract(5, 'hours').subtract(30, 'minutes');
     const endOfMonth = today.endOf('month').subtract(5, 'hours').subtract(30, 'minutes');
-    const workingDays = await calculateWorkingDay(newStartOfMonth, endOfMonth);
+    const workingDays = await calculateWorkingDay(startOfMonth, endOfMonth);
 
     const leaderboardParams = await LeaderboardParams.findOne({ status: 'Active', frequency: 'Monthly' })
     const data = await leaderboardDataHelper(startOfMonth, endOfMonth, leaderboardParams, workingDays);
@@ -1441,10 +1441,10 @@ exports.monthlyLeaderboardData = async (req, res) => {
 
 exports.quarterlyLeaderboardData = async (req, res) => {
   try {
-    const newStartOfQuarter = moment('2024-05-14').startOf('day').subtract(5, 'hours').subtract(30, 'minutes');
+    // const newStartOfQuarter = moment('2024-05-14').startOf('day').subtract(5, 'hours').subtract(30, 'minutes');
 
     const leaderboardParams = await LeaderboardParams.findOne({ status: 'Active', frequency: 'Quarter' });
-    const workingDays = await calculateWorkingDay(newStartOfQuarter, leaderboardParams?.quarterEndDate);
+    const workingDays = await calculateWorkingDay(leaderboardParams?.quarterStartDate, leaderboardParams?.quarterEndDate);
 
     const data = await leaderboardDataHelper(leaderboardParams?.quarterStartDate, leaderboardParams?.quarterEndDate, leaderboardParams, workingDays);
 
