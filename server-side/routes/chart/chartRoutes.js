96799edff5f4ff394ed12dab807b2ea1c6ab9888
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2024-05-28 08:36:23 +0530

    chart changes

--- server-side/routes/chart/chartRoutes.js
+++ server-side/routes/chart/chartRoutes.js
@@ -4,9 +4,9 @@ const Authenticate = require("../../authentication/authentication");
 const restrictTo = require("../../authentication/authorization");
 const router = express.Router();
 
-router
-  .route("/historical")
-  .get(Authenticate, chartController.getHistoricalData);
+router.route("/historical").get(chartController.getHistoricalData);
+router.route("/historicaladv").get(chartController.getHistoricalDataAdv);
+router.route("/allsymbols").get(chartController.getAllSymbols);
 router
   .route("/historicaludf")
   .get(Authenticate, chartController.getHistoricalDataUDF);
