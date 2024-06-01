290e2dd53181f5b650699b4dde563f6043c487c5
vijay <vvv201214@gmail.com>
2024-06-01 11:37:11 +0530

    staging chart

--- server-side/models/InstrumentHistoricalData/InstrumentHistoricalDataNew.js
+++ server-side/models/InstrumentHistoricalData/InstrumentHistoricalDataNew.js
@@ -5,32 +5,17 @@ const instrumentHistoricalDataSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    instrumentToken: {
-        type: String,
-        required: true
-    },
-    exchangeToken: {
-        type: String,
-        required: true
-    },
-    expiry: {
-        type: Date
-    },
-    candles: [
-        {
-            timestamp: Date,
-            open: Number,
-            high: Number,
-            close: Number,
-            low: Number,
-            volume: Number
-        }
-    ],
+    timestamp: Date,
+    open: Number,
+    high: Number,
+    close: Number,
+    low: Number,
+    volume: Number,
     createdOn: {
         type: Date,
         required: true
     }
 })
 
-const instrumentHistoricalData = mongoose.model("history-tick", instrumentHistoricalDataSchema);
+const instrumentHistoricalData = mongoose.model("history-tick-new", instrumentHistoricalDataSchema);
 module.exports = instrumentHistoricalData;
