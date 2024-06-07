87eda591fbd84c7166c1de70f11cbad4d52c9fce
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2024-06-07 09:49:08 +0530

    temp history collection

---
+++ server-side/models/InstrumentHistoricalData/InstrumentHistoricalDataTemp.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const instrumentHistoricalDataSchema = new mongoose.Schema({
+  symbol: {
+    type: String,
+    required: true,
+  },
+  instrumentToken: {
+    type: String,
+    required: false,
+  },
+  exchangeToken: {
+    type: String,
+    required: false,
+  },
+  expiry: {
+    type: Date,
+  },
+  candles: [
+    {
+      timestamp: Date,
+      open: Number,
+      high: Number,
+      close: Number,
+      low: Number,
+      volume: Number,
+    },
+  ],
+  createdOn: {
+    type: Date,
+    required: true,
+  },
+});
+
+const instrumentHistoricalData = mongoose.model(
+  "history-tick-temp",
+  instrumentHistoricalDataSchema
+);
+module.exports = instrumentHistoricalData;
--- server-side/routes/chart/chartRoutes.js
+++ server-side/routes/chart/chartRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const chartController = require("../../controllers/chartController");
 const Authenticate = require("../../authentication/authentication");
-const pnlChart = require('../../controllers/hourChart');
+const pnlChart = require("../../controllers/hourChart");
 const restrictTo = require("../../authentication/authorization");
+const { parseDataTemp } = require("../../controllers/historyDataHelper");
 const router = express.Router();
 
 router.route("/historical").get(chartController.getHistoricalData);
@@ -11,9 +12,12 @@ router.route("/allsymbols").get(chartController.getAllSymbols);
 router
   .route("/historicaludf")
   .get(Authenticate, chartController.getHistoricalDataUDF);
-  
-router.route('/uploadcsv').post(Authenticate, pnlChart.uploadMulter, pnlChart.uploadCSV);
-router.route('/hourly').get(Authenticate, pnlChart.hourChart);
-router.route('/isexist').get(Authenticate, pnlChart.isThirdPartyDataExist);
+
+router
+  .route("/uploadcsv")
+  .post(Authenticate, pnlChart.uploadMulter, pnlChart.uploadCSV);
+router.route("/hourly").get(Authenticate, pnlChart.hourChart);
+router.route("/isexist").get(Authenticate, pnlChart.isThirdPartyDataExist);
+router.route("/parsetemp").get(parseDataTemp);
 
 module.exports = router;
